8385d43c719e31372e8a4fd554aa909c0ef36e3f
Calvin Leung <github@myleungfamily.net>
2021-07-31 21:13:55 -0700

    add square

---
+++ square/square.ino
@@ -0,0 +1,72 @@
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+#define carSpeed 250
+
+void forward(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("go forward!");
+}G
+
+void back(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("go back!");
+}
+
+void left(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("go left!");
+}
+
+void right(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW); 
+  Serial.println("go right!");
+} 
+
+void setup() {
+
+}
+
+
+void loop() {
+  forward();
+  delay(500);
+  right()
+  delay(500);
+  forward();
+  delay(500);
+  right();;
+  delay(500);
+  forward();
+  delay(500);
+  right();
+  delay(500);
+  forward();
+  delay(500);
+  right();
+  delay(500);
+}
